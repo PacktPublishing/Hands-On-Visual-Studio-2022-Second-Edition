25452d005129429398b9090a23ba60d5ea6edd49
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2023-12-16 14:55:45 -0600

    Update BooksController.cs

--- Chapter 6/Chapter6_Code_API/Chapter6_Code_API/Controllers/BooksController.cs
+++ Chapter 6/Chapter6_Code_API/Chapter6_Code_API/Controllers/BooksController.cs
@@ -6,22 +6,21 @@ namespace Chapter6_Code_API.Controllers
     [Route("[controller]")]
     public class BooksController : ControllerBase
     {
-        // Simulando una base de datos en memoria
+        // Simulating an in-memory database
         private static readonly List<Book> Books = new List<Book>
     {
         new Book { Id = 1, Title = "1984", Author = "George Orwell", Genre = "Dystopia" },
-        new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Historical Fiction" },
-        // ... puedes añadir más libros para la demo
+        new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Historical Fiction" },        
     };
 
-        // Listar todos los libros
+        // List all books
         [HttpGet]
         public ActionResult<IEnumerable<Book>> Get()
         {
             return Books;
         }
 
-        // Obtener un libro específico por ID
+        // Get a specific book by ID
         [HttpGet("{id}")]
         public ActionResult<Book> GetBook(int id)
         {
@@ -33,16 +32,15 @@ namespace Chapter6_Code_API.Controllers
             return book;
         }
 
-        // Añadir un nuevo libro
+        // Add a new book
         [HttpPost]
         public ActionResult<Book> AddBook([FromBody] Book book)
-        {
-            // En una aplicación real, deberías asegurarte de que el ID es único, y probablemente lo generaría una base de datos.
+        {            
             Books.Add(book);
             return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
         }
 
-        // Actualizar un libro
+        // Update a book
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
@@ -58,7 +56,7 @@ namespace Chapter6_Code_API.Controllers
             return NoContent();
         }
 
-        // Eliminar un libro
+        // Delete a book
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
